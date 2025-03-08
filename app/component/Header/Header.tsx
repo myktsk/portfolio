5e1f9d2c0760bf5f61a763250d32fbad3c0b32f9
myktsk <22167483+myktsk@users.noreply.github.com>
2025-03-08 10:59:18 -0500

    :sparkles: global header base

---
+++ app/component/Header/Header.tsx
@@ -0,0 +1,81 @@
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
+import { useState } from "react";
+
+const NAVIGATION = [
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/miyukituisku/" },
+  { name: "GitHub", href: "https://github.com/myktsk" },
+  { name: "Email", href: "mailto:tuiskumiyuki@gmail.com" },
+];
+
+export const Header = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <header className="fixed inset-x-0 top-0 z-20">
+      <nav
+        aria-label="Global"
+        className="mx-auto flex max-w-7xl items-center justify-end p-6 lg:px-8"
+      >
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            onClick={() => setMobileMenuOpen(true)}
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon aria-hidden="true" className="size-6" />
+          </button>
+        </div>
+        <div className="hidden lg:flex">
+          {NAVIGATION.map((item, i) => (
+            <div key={item.name}>
+              {i > 0 && <span className="text-gray-500 mx-2">/</span>}
+              <a
+                href={item.href}
+                className="font-light text-sm/6 text-gray-900"
+                target="_blank"
+              >
+                {item.name}
+              </a>
+            </div>
+          ))}
+        </div>
+      </nav>
+      <Dialog
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+        className="lg:hidden"
+      >
+        <div className="fixed inset-0 z-10" />
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <button
+              type="button"
+              onClick={() => setMobileMenuOpen(false)}
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon aria-hidden="true" className="size-6" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                {NAVIGATION.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+        </DialogPanel>
+      </Dialog>
+    </header>
+  );
+};
